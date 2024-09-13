798ed0789021b9e6533950bb742bc5f29ed806cb
Eric Bariaux <375613+ebariaux@users.noreply.github.com>
2024-09-13 08:10:40 +0200

    We isolate the LED GPIO related code to its own struct.

--- LED/Main.swift
+++ LED/Main.swift
@@ -12,12 +12,22 @@
 @main
 struct Main {
   static func main() {
+    let led = Led()
+    while true {
+      led.toggle()
+      k_msleep(100)
+    }
+  }
+}
+
+struct Led {
+  init() {
     // Note: & in Swift is not the "address of" operator, but on a global variable declared in C
     // it will give the correct address of the global.
     gpio_pin_configure_dt(&led0, GPIO_OUTPUT | GPIO_OUTPUT_INIT_HIGH | GPIO_OUTPUT_INIT_LOGICAL)
-    while true {
-      gpio_pin_toggle_dt(&led0)
-      k_msleep(100)
-    }
+  }
+
+  func toggle() {
+     gpio_pin_toggle_dt(&led0)
   }
 }
